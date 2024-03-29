9225ef4513b701da24aa9dfc09112e3d1d6f3503
davis9001 <monaghan.david@gmail.com>
2024-03-29 11:06:45 -0700

    Black and white welcome with serif font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+  class="bg-black min-h-screen text-white p-9 font-serif flex items-center justify-center"
+>
+  <div class="container mx-auto">
+    <div class="max-w-sm md:max-w-5xl mx-auto">
+      <h1 class="text-9xl text-center">Welcome to Clarkdale</h1>
+    </div>
+  </div>
+</div>
